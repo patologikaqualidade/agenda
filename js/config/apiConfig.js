7dbf6dbd0e0c497cf3822d296a3dce7056dc7275
patologikaqualidade <patologikaqualidade@gmail.com>
2026-06-26 09:18:39 -0300

    Update apiConfig.js

--- js/config/apiConfig.js
+++ js/config/apiConfig.js
@@ -1,5 +1,5 @@
 // Cole aqui a URL do seu Web App do Google Apps Script após o deploy.
-export const API_URL = 'COLE_AQUI_A_URL_DO_APPS_SCRIPT';
+export const API_URL = 'https://script.google.com/macros/library/d/15-9EZzSMkkTDdz5Li50Al-oLBYwA133TFrjc70tROp-sN7378hY3_9iQ/2';
 
 export const SHEETS = {
   organizations:'Organizations', unidades:'Unidades', users:'Users', usuariosUnidades:'UsuariosUnidades',

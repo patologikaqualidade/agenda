2b32221235d0a90cfb83cb59eda17c8b0f37a2b6
patologikaqualidade <patologikaqualidade@gmail.com>
2026-06-26 09:30:27 -0300

    Update apiConfig.js

--- js/config/apiConfig.js
+++ js/config/apiConfig.js
@@ -1,9 +1,21 @@
-// Cole aqui a URL do seu Web App do Google Apps Script após o deploy.
-export const API_URL = 'https://script.google.com/macros/s/AKfycbz-TbJwKmtuHy3MfstK9Nbs1vQ4SLR2eT6MfzUHUSc5urdL7q-7N07Fpas5WYM6mNG-/exec';
+// URL do Web App do Google Apps Script
+export const API_URL = 'https://script.google.com/macros/s/AKfycbz-TbJwKmtuHy3MfstK9Nbs1vQ4SLR2eT6MfzUHUSc5urdL7q-7N07Fpas5WYM6mNG-/usercoderun';
 
 export const SHEETS = {
-  organizations:'Organizations', unidades:'Unidades', users:'Users', usuariosUnidades:'UsuariosUnidades',
-  permissoes:'PermissoesUsuarios', agendamentos:'Agendamentos', hospitais:'Hospitais', medicos:'Medicos',
-  convenios:'Convenios', procedimentos:'Procedimentos', status:'StatusAgendamento', motivos:'MotivosCancelamento',
-  bloqueios:'DatasBloqueadas', configAgenda:'ConfiguracoesAgenda', historico:'HistoricoAgendamentos', logs:'LogsAuditoria'
+  organizations:'Organizations', 
+  unidades:'Unidades', 
+  users:'Users', 
+  usuariosUnidades:'UsuariosUnidades',
+  permissoes:'PermissoesUsuarios', 
+  agendamentos:'Agendamentos', 
+  hospitais:'Hospitais', 
+  medicos:'Medicos',
+  convenios:'Convenios', 
+  procedimentos:'Procedimentos', 
+  status:'StatusAgendamento', 
+  motivos:'MotivosCancelamento',
+  bloqueios:'DatasBloqueadas', 
+  configAgenda:'ConfiguracoesAgenda', 
+  historico:'HistoricoAgendamentos', 
+  logs:'LogsAuditoria'
 };
